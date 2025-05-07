c69118a62413afde170a29c17a2de8135a5bd576
BarShira <barswisa41@gmail.com>
2025-05-07 15:19:12 +0300

    Delete src directory

--- src/game/items/Interactable.java
+++
@@ -1,9 +0,0 @@
-// Students: Aviv Nahum, ID : 206291163 -- Bar Swisa, ID: 211631551
-
-package game.items;
-import game.characters.PlayerCharacter;
-
-public interface Interactable {
-    void interact(PlayerCharacter c);
-
-}
